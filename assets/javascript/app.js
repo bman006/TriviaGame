2e05daca6146192dae7122626f0ae771bee5e34c
Brian Parkes <parkesbrianj@gmail.com>
2017-12-28 12:09:03 -0600

    Working on how to stop timer when an answer is selected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ var TriviaGame = {
 	//Store question and answer information
 	questions: [
 		{
-			question: "Who was given the title &quot;Full Metal&quot; in the anime series &quot;Full Metal Alchemist&quot;?",
+			question: 'Who was given the title "Full Metal" in the anime series "Full Metal Alchemist"?',
 			correctAnswer: "Edward Elric", 
 			wrongAnswer: ["Alphonse Elric", "Van Hohenheim", "Izumi Curtis"]
 		},
@@ -26,17 +26,18 @@ var TriviaGame = {
 			wrongAnswer: ['wrong 1', 'wrong 2',	'wrong 3']
 		}
 	],
-	timePerQuestion: 	12, 		//Units in seconds, cannot exceed 99
+	timePerQuestion: 	10000, 		//Units in seconds, cannot exceed 99
 	questionCounter: 	0,			//Counter for which question is current
 	answerIndexCounter: 0,			//Counter for master answer index attribute
 	correctAnswers: 	0,			//Tally for total questions answered correctly
 	incorrectAnswers: 	0,			//Tally for total questions answered incorrectly
 	timeOuts: 			0, 			//Tally for total questions not answered before time out
+	timedOut: 			false,		//For use when timer runs out in resolveQuestion function
 
 	initialize: function() {
+		this.timedOut = false;
 		this.nextQuestion(this.questions, this.questionCounter);
 		this.runTimer();
-		this.questionCounter++;
 	},
 
 	nextQuestion: function(questions, questionCounter) {
@@ -61,7 +62,6 @@ var TriviaGame = {
 		var newQuestionBox 			= $('<div>').addClass('question-box');
 		var questionSlot 			= $('<div>').addClass('question-prompt').text(this.questions[this.questionCounter].question);
 		var timeSlot 				= $('<div>').addClass('time-display');
-		var submitButton			= $('<button>').addClass('btn btn-submit').text('submit');
 		var answerList 				= $('<ol>').attr('answer-selected', 'no');
 
 		answerList.addClass('answer-list').attr('type', 'a').attr('question-number',this.questionCounter);
@@ -79,42 +79,51 @@ var TriviaGame = {
 			.append(questionSlot)
 			.append(timeSlot)
 			.append(answerList)
-			.append(submitButton);
 
-		$('main').append(newQuestionBox);
+		$('main').prepend(newQuestionBox);
 
 		//Add event handlers for when selecting answers
 		$('.btn-answer').focus(function() {
-			$('ol[question-number='+TriviaGame.questionCounter+']').attr('answer-selected', 'yes');
+			// $('ol[question-number='+TriviaGame.questionCounter+']').attr('answer-selected', 'yes');
+			$(this).attr('current-selection', 'yes');
+			TriviaGame.resolveQuestion();
 		});
-		$('.btn-answer').focusout(function() {
-			$('ol[question-number='+TriviaGame.questionCounter+']').attr('answer-selected', 'no');
-		});
+		// $('.btn-answer').focusout(function() {
+		// 	// $('ol[question-number='+TriviaGame.questionCounter+']').attr('answer-selected', 'no');
+		// 	$(this).attr('current-selection', 'no');
+		// });
 	},
 
 	runTimer: function() {
-		var timerDisplay = $('div.time-display').eq(this.questionCounter);
+		var obj = this;
+		var timerDisplay = $('div.time-display');
 		var timeLeft = this.timePerQuestion;
 		var intervalId = setInterval(function(){
 			if (timeLeft >= 0) {
-				timerDisplay.text(TriviaGame.timeConverter(timeLeft));
+				timerDisplay.html('Seconds left: <br>' + timeLeft);
 				timeLeft--;
 			}
 			else {
 				clearInterval(intervalId);
-				TriviaGame.answerTimeOut();
+				obj.timedOut = true;
+				obj.resolveQuestion();
 			}
 		}, 1000);
 	},
 
-	timeConverter: function(t) {
-	    if (t < 10) {
-	      t = "0" + t;
-	    }
-	    return t;
-	},
-	submitAnswer: function() {
+	//This function handles logic for checking answer correctness based on either user submission or timeout conditions
+	resolveQuestion: function() {
+		//stop timer
 
+
+		//get answer
+
+		//check if answer is correct
+
+		//update tallys for correct/incorrect answers
+
+		this.questionCounter++;
+		this.initialize();
 	},
 	answerCorrectly: function(){
 		correctAnswers++;
