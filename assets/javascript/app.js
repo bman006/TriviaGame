8f0ca2a5e25be436ad840a96324ead7da12a4d25
Brian Parkes <parkesbrianj@gmail.com>
2017-12-30 10:30:51 -0600

    standardized obj versus this throughout functions, seperated buttn reformat from resolve function for easier reading, deleted some more commented out code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,36 +36,37 @@ var TriviaGame = {
 	intervalId: 		0,			//Placeholder key so updateTimerDisplay() interval can be cleared globally
 	timeOuts: 			0, 			//Tally for total questions not answered before time out
 	timedOut: 			false,		//For use when timer runs out in resolveQuestion function
-	screenFreeze: 		false,		//When true, applicable setTimeout functions will halt progress
 
+	initialize: function() {
+		var obj = this;
+		obj.timedOut = false;
+		obj.createQuestionBox(obj);
+		// this.scrollToTop();
+		window.scrollTo(0,0);
+		var timerType = 'question';
+		obj.runTimer(obj, timerType);
+	},
+
+	//Clear whatever interval is currently running
 	stopAnyInterval: function() {
 		clearInterval(this.intervalId);
 	},
 
-	initialize: function() {
-		this.timedOut = false;
-		this.nextQuestion(this.questions, this.questionCounter);
-		// this.scrollToTop();
-		window.scrollTo(0,0);
-		var timerType = 'question';
-		this.runTimer(timerType);
-	},
-
 	//Generate HTML for container with question, answers, and timer
-	createQuestionBox: function() {
+	createQuestionBox: function(obj) {
 		//Create element structure
 		var newQuestionBox 			= $('<div>').addClass('question-box');
-		var questionSlot 			= $('<div>').addClass('question-prompt').text(this.questions[this.questionCounter].question);
-		var timeSlot 				= $('<div>').addClass('time-display').attr('question-number',this.questionCounter);
+		var questionSlot 			= $('<div>').addClass('question-prompt').text(obj.questions[obj.questionCounter].question);
+		var timeSlot 				= $('<div>').addClass('time-display').attr('question-number',obj.questionCounter);
 		var answerList 				= $('<ol>').attr('answer-selected', 'no');
 
-		answerList.addClass('answer-list').attr('type', 'a').attr('question-number',this.questionCounter);
+		answerList.addClass('answer-list').attr('type', 'a').attr('question-number',obj.questionCounter);
 
 		//Create attributes to reference element based on which button is selected by user
 		var AnswerListItems = '';
-		for (i = 0; i < this.numAnswers; i++) {
-			AnswerListItems += '<li><button class="btn btn-answer" answer-index='+this.answerIndexCounter+' active-answer="yes" current-selection="no"></button></li>';
-			this.answerIndexCounter++;
+		for (i = 0; i < obj.numAnswers; i++) {
+			AnswerListItems += '<li><button class="btn btn-answer" answer-index='+obj.answerIndexCounter+' active-answer="yes" current-selection="no"></button></li>';
+			obj.answerIndexCounter++;
 		}
 
 		answerList.append(AnswerListItems);
@@ -78,27 +79,26 @@ var TriviaGame = {
 		$('main').prepend(newQuestionBox);
 
 		//Add event handlers for when selecting answers
-		$('.btn-answer').focus(function() {
-			// $('ol[question-number='+TriviaGame.questionCounter+']').attr('answer-selected', 'yes');
+		$('.btn-answer').click(function() {
 			$(this).attr('current-selection', 'yes');
-			TriviaGame.resolveQuestion();
+			TriviaGame.resolveQuestion(obj);
 		});
-		// $('.btn-answer').focusout(function() {
-		// 	// $('ol[question-number='+TriviaGame.questionCounter+']').attr('answer-selected', 'no');
-		// 	$(this).attr('current-selection', 'no');
-		// });
+		obj.nextQuestion(obj);
 	},
 
 	//Insert answer text into buttons after generating question container
-	nextQuestion: function(questions, questionCounter) {
-		this.createQuestionBox(this.numAnswers);
-		//Insert answer options
-		var correctAnswerSlot = Math.floor(Math.random() * this.numAnswers);
+	nextQuestion: function(obj) {
+		var questions = obj.questions;
+		var questionCounter = obj.questionCounter;
 
+		//Generate number to randomly select which button has the correct answer
+		var correctAnswerSlot = Math.floor(Math.random() * obj.numAnswers);
+		//Insert the correct answer text into the HTML
 		$('ol[question-number='+questionCounter+']').children().children().eq(-1*correctAnswerSlot).text(questions[questionCounter].correctAnswer);
 
+		//Load the incorrect answers into the remaining buttons
 		var j = 0;
-		for (var i=0; i < this.numAnswers; i++) {
+		for (var i=0; i < obj.numAnswers; i++) {
 			if($('ol[question-number='+questionCounter+']').children('li').children('button').eq(i).text() === "") {
 				$('ol[question-number='+questionCounter+']').children('li').children('button').eq(i).text(questions[questionCounter].wrongAnswer[j]);
 				j++;
@@ -107,25 +107,22 @@ var TriviaGame = {
 	},
 
 	//Set up the timer based on if user is in the question or review stage, and which question is current
-	runTimer: function(timerType) {
-		var obj = this;
-		var timerDisplay = $('div.time-display[question-number='+this.questionCounter+']');
+	runTimer: function(obj, timerType) {
+		var timerDisplay = $('div.time-display[question-number='+obj.questionCounter+']');
 		var timerText;
 		if (timerType === 'question') {
-			var timeLeft = this.timePerQuestion;
+			var timeLeft = obj.timePerQuestion;
 			timerText = 'Seconds left: <br>';
 		}
 		else if (timerType === 'review') {
-			var timeLeft = this.timeToReview;
+			var timeLeft = obj.timeToReview;
 			timerText = 'Next question in: <br>';
 		}
-		this.updateTimerDisplay(timerType, timeLeft, timerDisplay, timerText);
+		obj.updateTimerDisplay(obj, timerType, timeLeft, timerDisplay, timerText);
 	},
 
 	//
-	updateTimerDisplay: function(timerType, timeLeft, timerDisplay, timerText) {
-		var obj = this;
-
+	updateTimerDisplay: function(obj, timerType, timeLeft, timerDisplay, timerText) {
 		//Interval for answering a question
 		obj.stopAnyInterval();
 		obj.intervalId = setInterval(function(){
@@ -147,24 +144,10 @@ var TriviaGame = {
 		}, 1000);
 	},
 
-	//Scroll to top of screen for next question
-	scrollToTop: function() {
-		var obj = this;
-		obj.stopAnyInterval();
-		obj.intervalId = setInterval(function(){
-			if (window.scrollY > 5) {
-				window.scrollBy(0,-5);
-			}
-			else {
-				window.scrollTo(0,0);
-				obj.stopAnyInterval();
-			}
-		}, 10);
-	},
+
 
 	//This function handles logic for checking answer correctness based on either user submission or timeout conditions
-	resolveQuestion: function() {
-		var obj = this;
+	resolveQuestion: function(obj) {
 		//stop timer
 		obj.stopAnyInterval();
 		
@@ -198,6 +181,18 @@ var TriviaGame = {
 		}
 		
 		//Reformat answer buttons based on answer result
+		obj.reformatButtons(obj, currentQuestion, currentAnswers, answer);
+
+		//Update timer to show how long until the next questions
+		var timerType = 'review';
+		obj.runTimer(obj, timerType);
+		//Check if there are any questions left to ask
+			//Reinitialize the game
+			//or finish the game
+	},
+
+	//Reformat buttons styles depending on correct, incorrect, time out conditions
+	reformatButtons: function(obj, currentQuestion, currentAnswers, answer) {
 		//get the number of which attribute number to start with	
 		var startingAnswer = obj.questionCounter * obj.numAnswers;			
 		for (i = 0; i < obj.numAnswers; i++) {
@@ -216,15 +211,28 @@ var TriviaGame = {
 			}
 			startingAnswer++;
 		}
-
-		//Update timer to show how long until the next questions
-		var timerType = 'review';
-		obj.screenFreeze = true;
-		obj.runTimer(timerType);
-		//Check if there are any questions left to ask
-			//Reinitialize the game
-			//or finish the game
 	},
+
+	//When there are no more questions.....
+	noMoreQuestions: function(obj) {
+
+	},
+
+//I haven't been able to get to the bottom of why this doesn't work when I call it in the initialize function
+	//Scroll to top of screen for next question
+	// scrollToTop: function() {
+	// 	var obj = this;
+	// 	obj.stopAnyInterval();
+	// 	obj.intervalId = setInterval(function(){
+	// 		if (window.scrollY > 5) {
+	// 			window.scrollBy(0,-5);
+	// 		}
+	// 		else {
+	// 			window.scrollTo(0,0);
+	// 			obj.stopAnyInterval();
+	// 		}
+	// 	}, 10);
+	// },
 }
 
 TriviaGame.initialize();
